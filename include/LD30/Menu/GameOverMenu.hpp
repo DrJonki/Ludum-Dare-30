286222da71a5a05ffa031253b182a144abd5f12a
Joona Tiinanen <joona_tiinanen@hotmail.com>
2014-08-25 15:05:05 +0300

    Fixed merge again

--- include/LD30/Menu/GameOverMenu.hpp
+++ include/LD30/Menu/GameOverMenu.hpp
@@ -5,10 +5,22 @@
 
 namespace ld
 {
-    class gameOverMenu : public BaseMenu
+    class GameOverMenu : public BaseMenu
     {
     public:
 
+        GameOverMenu(sf::RenderWindow& window);
+
+        ~GameOverMenu() final override;
+
+
+        void fadeInStep(const float delta) final override;
+
+        void fadeOutStep(const float delta) final override;
+
+        void update(const float delta) final override;
+
+        void draw() final override;
 
     };
 }
---
+++ src/LD30/Menu/GameOverMenu.cpp
@@ -0,0 +1,34 @@
+#include <LD30/Menu/GameOverMenu.hpp>
+
+
+ld::GameOverMenu::GameOverMenu(sf::RenderWindow& window)
+    : BaseMenu(window)
+{
+
+}
+
+ld::GameOverMenu::~GameOverMenu()
+{
+
+}
+
+void ld::GameOverMenu::fadeInStep(const float )
+{
+    
+}
+
+void ld::GameOverMenu::fadeOutStep(const float )
+{
+    
+}
+
+void ld::GameOverMenu::update(const float )
+{
+    
+}
+
+void ld::GameOverMenu::draw()
+{
+    
+}
+
